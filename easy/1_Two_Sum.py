1f80979cac29242aca3f20d7cb9320e98e3d94ff
Mohamed Amine Jabri <mjabri@student.1337.ma>
2026-05-07 13:45:50 +0100

    leetcode

--- easy/1_Two_Sum.py
+++ easy/1_Two_Sum.py
@@ -1,4 +1,4 @@
-from typing import List # i added this cause i'm wrking in vscode
+from typing import List # add this to test with vscode
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
--- easy/20_Valid_Parentheses.py
+++ easy/20_Valid_Parentheses.py
@@ -1,11 +1,14 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
-    def isValid(self, s: str) -> bool:
-        brak = {'{': '}', '[': ']', '(' : ')' }
-        openings = []
-        for c in s:
-            if c in brak:
-                openings.append(c)
-            elif c in brak.values():
-                if not openings or c != brak[openings.pop()]:
-                    return False
-        return not openings
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        return sorted(list1 + list2)
+
+test = Solution()
+print(test.mergeTwoLists([1,2,4], [1,3,4]))
